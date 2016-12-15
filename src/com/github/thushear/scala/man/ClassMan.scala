62b8664470f685f16f831326589f35f3bb466ca8
kongming <kongming@jd.com>
2016-12-15 19:43:01 +0800

    Initial commit

---
+++ src/com/github/thushear/scala/man/ClassMan.scala
@@ -0,0 +1,17 @@
+package com.github.thushear.scala.man
+
+/**
+  * Created by kongming on 2016/12/15.
+  */
+class ClassMan {
+
+  private var name = "thushear";
+
+  def sayHi(): Unit ={
+    println("hello " + name)
+  }
+
+  def getName = name
+
+
+}
---
+++ src/com/github/thushear/scala/man/ScalaMan.scala
@@ -0,0 +1,14 @@
+package com.github.thushear.scala.man
+
+/**
+  * Created by kongming on 2016/12/15.
+  */
+object ScalaMan {
+
+  def main(args: Array[String]): Unit = {
+//    println("hello world!")
+    val  c = new ClassMan
+    c.sayHi()
+    println(c.getName)
+  }
+}
