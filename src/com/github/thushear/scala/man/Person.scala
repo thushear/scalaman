0e4183a37c4b50601bab8dfc25bf7e78168b5a00
kongming <kongming@jd.com>
2016-12-20 09:02:07 +0800

    scala

--- src/com/github/thushear/scala/man/Person.scala
+++ src/com/github/thushear/scala/man/Person.scala
@@ -3,6 +3,43 @@ package com.github.thushear.scala.man
 /**
   * Created by kongming on 2016/12/15.
   */
+trait Hanlder{
+  def handle(data:String){}
+}
+trait CheckHandler extends Hanlder{
+  override def handle(data: String): Unit = {
+    println("data check handle")
+    super.handle(data)
+  }
+}
+
+trait SignatureHandler extends Hanlder{
+  override def handle(data: String): Unit = {
+    println("signature handle")
+    super.handle(data)
+  }
+}
+
+trait Logger{
+  def log(message:String) = println(message)
+}
+trait Logged{
+  def logged(message:String){}
+}
+
+trait MixinLogger extends Logged{
+  override def logged(message: String): Unit = {
+    println("log:" + message)
+  }
+}
+
+trait HiTrait{
+  def sayHi(name: String)
+}
+trait MakeFriendsTrait{
+  def makeFriend(p:Person)
+}
+
 object Person {
 
 
@@ -15,12 +52,23 @@ object Person {
 
   def apply(name: String = "thushear", age: Int = 10): Person = new Person(name, age)
 }
-class Person(name:String = "thushear",age:Int = 10){
+class Person(name:String = "thushear",age:Int = 10) extends HiTrait with MakeFriendsTrait with Cloneable with Serializable with Logger with Logged with CheckHandler with SignatureHandler
+{
   def getName=name
 
   def sayHi(): Unit ={
     println("my name is " + name + " age is " + age + " has eyes " + Person.eyeNum)
   }
+
+  override def sayHi(name: String): Unit = {
+    println("say hi to " + name)
+    log("say hi is invoked " + name)
+    logged("hi logged mixedin")
+    //Trait调用链
+    handle("handler data " + name)
+  }
+
+  override def makeFriend(p: Person): Unit = println("my name is " + name + "  frient name is " + p.getName)
 }
 
 class Employee extends Person{
--- src/com/github/thushear/scala/man/ScalaMan.scala
+++ src/com/github/thushear/scala/man/ScalaMan.scala
@@ -1,5 +1,7 @@
 package com.github.thushear.scala.man
 
+import javax.swing._
+import java.awt.event._
 /**
   * Created by kongming on 2016/12/15.
   */
@@ -48,5 +50,89 @@ object ScalaMan {
       ee.getName
     }
 
+    val pppp = Person("thus")
+    val p5 = Person("hear")
+    pppp.sayHi("trait")
+    pppp.makeFriend(p5)
+
+    val p6 = new Person("ttt") with MixinLogger
+    p6.sayHi("mixin")
+
+    //函数式编程
+    def doSomething(name:String){
+      println("doSomeThing " + name)
+    }
+
+    val doSomethingFunc = doSomething _
+
+    doSomethingFunc("some")
+
+    //匿名函数
+    val doAnotherthingFunc = (name:String)=>println("doAnotherthingFunc " + name)
+    doAnotherthingFunc("another")
+
+    //高阶函数
+    def greeting(func:(String)=>Unit,name:String): Unit ={
+      func(name)
+    }
+    greeting(doSomethingFunc,"leo")
+
+    println(Array(1,2,4,6).map((num:Int)=>num * num).toString)
+
+
+    println(Array(1,2,3,4,5,6,7).map(2 * _).toString)
+
+    Array(1,2,4,6).map((num:Int)=>num * num).foreach(println _)
+    Array(1,2,3,4,5,6,7).map(2 * _).foreach(println _)
+
+    (1 to 9).map("*" * _).foreach(println _)
+
+    (1 to 20).filter(_ % 2 == 0).foreach(println _)
+
+    (1 to 11).reduceLeft(_ * _).toString
+
+    //闭包
+    def getGreeting(msg:String) = (name:String) => println(msg + " to " + name)
+    val greetingFunc = getGreeting("hello")
+    greetingFunc("bibao")
+
+    val button = new JButton("click")
+    button.addActionListener(new ActionListener {
+      override def actionPerformed(e: ActionEvent): Unit = {
+        println("click me!!!")
+      }
+    })
+
+    def sum(a:Int) = (b:Int) => a + b
+    val summ = sum(1)(2)
+    println(summ)
+
+    def decorator(l:List[Int],prefix:String): Unit ={
+      if (l != Nil){
+        println(prefix + l.head)
+        decorator(l.tail,prefix)
+      }
+    }
+    decorator(List(1,2,3,4,5),"hi ")
+
+    val list = scala.collection.mutable.LinkedList(1,2,3,4,5)
+    var currentList = list
+    while(currentList != Nil){
+      currentList.elem = currentList.elem * 2
+      currentList = currentList.next
+    }
+    println(currentList)
+    println(list)
+
+    println("===========================")
+    List("hello world","thushear github").flatMap(_.split(" ")).foreach(println _)
+
+    List("thusher","leo").zip(List(100,88)).foreach(println _)
+
+    println("===========================")
+    val line = scala.io.Source.fromFile("D:\\codingnet\\scalaman\\src\\resources\\input.txt").mkString
+    println(List(line).flatMap(_.split(" ")).map((_,1)).map(_._2).reduceLeft(_+_))
+    List(line).flatMap(_.split(" ")).map((_,1)).foreach(println _)
+    List(line).flatMap(_.split(" ")).map((_,1)).map(_._2).foreach(println _)
   }
 }
